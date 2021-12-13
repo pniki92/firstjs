89080ed7c548b15c6f9e0ca2e2e08516fa0ac850
Istvan <istvan_gyarmati@epam.com>
2021-12-13 09:33:14 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const movies = [
+    {
+        title: "Harry Potter 1",
+        release_date: "2010-05-02",
+        rating_avg: 9.2,
+    },
+    {
+        title: "Harry Potter 2",
+        release_date: "2011-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 3",
+        release_date: "2012-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 4",
+        release_date: "2013-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 5",
+        release_date: "2014-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 6",
+        release_date: "2015-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 7-1",
+        release_date: "2016-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+    {
+        title: "Harry Potter 7-2",
+        release_date: "2017-05-02",
+        rating_avg: 9.2,
+        genres: ["Fantasy", "Drama"]
+    },
+]
+
+// TODO: display all records above in the html with javascript target: all-movies-wrapper
+// TODO2: extend the objects with the movie's cover image link display them also. target: all-movies-with-img-wrapper
+// TODO3: filter the object with js to display only movies between 2014-2016. target: filtered-movies-with-img-wrapper
